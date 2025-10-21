54714492e2256c766bf53f27e4c0017fadf72e8b
normal-newby <nimo.r.liu@gmail.com>
2025-10-21 18:41:34 -0400

    added principles

--- javascript/colours.js
+++ javascript/colours.js
@@ -1,6 +1,7 @@
 const colours = {
     finance: "rgb(147, 255, 143)",
-    marketing: "rgb(100,100,100)"
+    marketing: "rgb(100,100,100)",
+    principles: "rgb(255,255,255)"
 }
 for (let colour in colours){
     cell = document.querySelector(`.${colour}`);
